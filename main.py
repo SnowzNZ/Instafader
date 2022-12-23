89b52c4bd97c3626457e5f044a195a92266ee7e3
SnowzNZ <jackson.baylis@outlook.co.nz>
2022-12-23 20:50:57 +1300

    Linter issues

--- main.py
+++ main.py
@@ -1,24 +1,29 @@
+"""
+Makes osu! skins instafade.
+"""
+
 import configparser
-import pick
-import shutil
-import numpy as np
 import os
+import shutil
+
+import numpy as np
+import pick
 from PIL import Image
 
 config = configparser.ConfigParser(strict=False, allow_no_value=True)
 config.read("skin.ini", encoding="utf8")  # read skin.ini
 
 try:
-    vars = config.options("Colours")  # check for colours section in skin
-except Exception as NoSectionError:  # if not
+    colors = config.options("Colours")  # check for colours section in skin
+except TypeError:  # if not
     print("No Colours found")  # tell the user no colors are found
     input()  # pause
 
 
 options = []
 
-# get rgb value from vars
-for i in vars:
+# get rgb value from colors
+for i in colors:
     options.append(f"{i.capitalize()}: {config.get('Colours', i)}")
 
 option = pick.pick(options, indicator=">")  # pick option
@@ -54,8 +59,10 @@ for a in range(10):
         f"{config['Fonts']['HitCirclePrefix']}-{a}.png",
         f"before-instafader/default-{a}.png",
     )
-shutil.copyfile("hitcircle.png", f"before-instafader/hitcircle.png")
-shutil.copyfile("hitcircleoverlay.png", f"before-instafader/hitcircleoverlay.png")
+shutil.copyfile("hitcircle.png", "before-instafader/hitcircle.png")
+shutil.copyfile(
+    "hitcircleoverlay.png", "before-instafader/hitcircleoverlay.png"
+)
 
 # paste number on top of the hitcircles and save them
 for n in range(10):
@@ -64,6 +71,7 @@ for n in range(10):
     x, y = paste.size
     a, b = number.size
     paste.paste(number, (x // 2 - a // 2, y // 2 - b // 2))
+    # paste.resize(160, 160, Image.Resampling.LANCZOS)
     paste.save(f"default-{n}.png")
 
 os.remove("no-number.png")  # remove no number image as we are done with it
@@ -74,7 +82,9 @@ blank_image.save("hitcircle.png")
 blank_image.save("hitcircleoverlay.png")
 
 config["Fonts"]["HitCirclePrefix"] = "default"
-config["Fonts"]["HitCircleOverlap"] = x  # update hitcircleoverlap in skin.ini
+config["Fonts"]["HitCircleOverlap"] = str(
+    x
+)  # update hitcircleoverlap in skin.ini
 
-with open("skin.ini", "w") as configfile:  # save everything
-    config.write(configfile)
+with open("skin.ini", "w", encoding="utf-8") as f:  # save everything
+    config.write(f)
