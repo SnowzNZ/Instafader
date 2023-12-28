aabce57524d5e664d32b6d613a627673e082284d
Snowz <98007575+SnowzNZ@users.noreply.github.com>
2023-12-28 16:06:12 +1300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,65 +1,57 @@
-"""
-Makes osu! skins instafade.
-"""
-import configparser
 import os
-import shutil
+import datetime
+from tkinter import filedialog
+import configparser
 import sys
-
-import numpy as np
+import shutil
+from PIL import Image, ImageChops
 import pick
-from PIL import Image, ImageDraw
 
-config = configparser.ConfigParser()
+# Ask for skin folder
+skin_folder = filedialog.askdirectory(title="Select Skin Folder")
 
-backup_folder = "instafader-backup"
+# Change directory to skin folder
+os.chdir(skin_folder)
 
-# Check if a backup folder doesnt exist
-if not os.path.exists(backup_folder):
-    os.mkdir(backup_folder)  # Create backup folder
+# Create backup folder
+backup_folder = (
+    f"Instafader-Backup-{datetime.datetime.now().strftime('%Y-%m-%d-%H-%M-%S')}"
+)
+os.mkdir(backup_folder)
+print("Creating backup folder")
 
-# Read the skin.ini file
+# Initialize configparser
+config = configparser.ConfigParser(
+    allow_no_value=True,
+    comment_prefixes="//",
+    delimiters=":",
+    inline_comment_prefixes="//",
+    strict=False,
+)
+
 try:
-    config.read("skin.ini")
+    # Create backup of skin.ini
     shutil.copy2("skin.ini", f"{backup_folder}/skin.ini")
+
+    # lstrip(" \t") each line
+    with open("skin.ini", encoding="utf-8") as f:
+        lines = [line.lstrip(" \t") for line in f]
+
+    # Join all lines and read into configparser
+    config.read_string("\n".join(lines))
+
 except FileNotFoundError:
     print("No skin.ini found!")
     sys.exit()
-except PermissionError:
-    print(
-        "The program does not have permission to read skin.ini! Try running it as admin."
-    )
-    sys.exit()
-
-options = []
-
-# Get all colors in the section
-try:
-    # Get all colors
-    section_keys = config.options("Colours")
-
-    # Filter out all colors that arent Combo colors and add them to the options that the user can choose from
-    options = [
-        f"{i.capitalize()}: {config.get('Colours', i)}"
-        for i in (key for key in section_keys if "combo" in key.lower())
-    ]
-
-    # Ask the user to pick a Combo color
-    option = pick.pick(options, indicator=">")
-
-except configparser.NoSectionError:
-    print(
-        "The skin.ini provided does not contain a Colours section, the hitcircle will NOT be colored."
-    )
 
 # Try to open the @2x hitcircle, if it cant be found then try to open the SD hitcircle
 try:
     hitcircle = Image.open("hitcircle@2x.png")
-    shutil.copy2(f"hitcircle@2x.png", f"{backup_folder}/hitcircle@2x.png")
+    shutil.copy2("hitcircle@2x.png", f"{backup_folder}/hitcircle@2x.png")
 except FileNotFoundError:
     try:
         hitcircle = Image.open("hitcircle.png")
-        shutil.copy2(f"hitcircle.png", f"{backup_folder}/hitcircle.png")
+        shutil.copy2("hitcircle.png", f"{backup_folder}/hitcircle.png")
     except FileNotFoundError:
         # Alert the user if no hitcircle image could be found
         print("No hitcircle could be found in the provided skin!")
@@ -68,108 +60,120 @@ except FileNotFoundError:
 # Try to open the @2x hitcircleoverlay, if it cant be found then try to open the SD hitcircleoverlay
 try:
     hitcircleoverlay = Image.open("hitcircleoverlay@2x.png")
-    shutil.copy2(
-        f"hitcircleoverlay@2x.png", f"{backup_folder}/hitcircleoverlay@2x.png"
-    )
+    shutil.copy2("hitcircleoverlay@2x.png", f"{backup_folder}/hitcircleoverlay@2x.png")
 except FileNotFoundError:
     try:
         hitcircleoverlay = Image.open("hitcircleoverlay.png")
-        shutil.copy2(
-            f"hitcircleoverlay.png", f"{backup_folder}/hitcircleoverlay.png"
-        )
+        shutil.copy2("hitcircleoverlay.png", f"{backup_folder}/hitcircleoverlay.png")
     except FileNotFoundError:
         # Alert the user if no hitcircle image could not be found
         print("No hitcircleoverlay could be found in the provided skin!")
         sys.exit()
 
-# Color the hitcircle
-try:
-    string, zero = option
-    label, rgb_string = string.split(":")
-    rgb_list = rgb_string.split(",")
-    r, g, b = (int(rgb_list[0]), int(rgb_list[1]), int(rgb_list[2]))
-except:
-    r, g, b = (255, 255, 255)
+options = []
 
-width, height = hitcircle.size
+for option in config["Colours"]:
+    if option.startswith("combo"):
+        options.append((option, config["Colours"][option]))
 
-for x in range(width):
-    for y in range(height):
-        # Get the pixel at the current position
-        pixel = hitcircle.getpixel((x, y))
-        # Calculate the brightness of the pixel
-        brightness = sum(pixel) / 3
-        # Calculate the amount of desaturation based on the brightness
-        desaturation = int(brightness * 0.5)
-        # Choose a color for the pixel
-        color = (255, 0, 0)  # red
-        # Create a new color with reduced saturation
-        desaturated_color = tuple(c - desaturation for c in color)
-        # Set the pixel to the new color
-        hitcircle.putpixel((x, y), desaturated_color)
+option, _ = pick.pick(options=options, title="Choose color")
 
-hitcircle.save("recolored_circle.png")
+rgb = tuple(map(int, option[1].split(",")))
 
-# hitcircle = hitcircle.point(lambda x: (x, x, x, 255))
-# hitcircle.save("overlayed.png")
+solid_color = Image.new("RGBA", (hitcircle.width, hitcircle.height), rgb)
 
-# # recoloring
-# im = hitcircle.convert("RGBA")
-# data = np.array(im)
-# red, green, blue, alpha = data.T
-# white_areas = (red == 255) & (blue == 255) & (green == 255)
-# string, zero = option
-# label, rgb_string = string.split(":")
-# r, g, b = rgb_string.split(",")
-# rgb = int(r), int(g), int(b)
-# data[..., :-1][white_areas.T] = rgb
-# background = Image.fromarray(data)
+hitcircle = ImageChops.multiply(hitcircle.convert("RGBA"), solid_color.convert("RGBA"))
+hitcircle = hitcircle.resize(
+    (int(hitcircle.width * 1.25), int(hitcircle.height * 1.25)), resample=Image.LANCZOS
+)
 
-# Paste the hitcircleoverlay on top of the hitcircle
-hitcircle.paste(hitcircleoverlay, (0, 0), hitcircleoverlay)
+hitcircleoverlay = hitcircleoverlay.resize(
+    (int(hitcircleoverlay.width * 1.25), int(hitcircleoverlay.height * 1.25)),
+    resample=Image.LANCZOS,
+)
+
+
+if hitcircle.size < hitcircleoverlay.size:
+    # Create a new image with the desired canvas size
+    no_number = Image.new(
+        "RGBA", hitcircleoverlay.size, (255, 255, 255, 0)
+    )  # Use a white background
+
+    # Calculate the position to paste the original image centered on the new canvas
+    paste_position = (
+        (hitcircleoverlay.width - hitcircle.width) // 2,
+        (hitcircleoverlay.height - hitcircle.height) // 2,
+    )
+
+    # Paste the original image onto the new canvas
+    no_number.paste(hitcircle, paste_position)
+    no_number.paste(hitcircleoverlay, (0, 0), hitcircleoverlay)
+
+elif hitcircle.size > hitcircleoverlay.size:
+    no_number = Image.new("RGBA", hitcircle.size, (255, 255, 255, 0))
+
+    paste_position = (
+        (hitcircle.width - hitcircleoverlay.width) // 2,
+        (hitcircle.height - hitcircleoverlay.height) // 2,
+    )
+
+    no_number.paste(hitcircleoverlay, paste_position)
+    no_number.paste(hitcircle, (0, 0), hitcircle)
+else:
+    no_number = hitcircle.paste(hitcircleoverlay, (0, 0), hitcircleoverlay)
+
+no_number.save("no-number.png")
+
 
 # Get the prefix of the hitcircle number
-hitcircle_prefix = config.get("Fonts", "HitCirclePrefix")
+hitcircle_prefix = config["Fonts"]["HitCirclePrefix"]
 
 # Check if all the hitcircle numbers exist
 for i in range(10):
     try:
         # Try to open the @2x hitcircle number
         number = Image.open(f"{hitcircle_prefix}-{i}@2x.png")
+        hd = True
         # Copy the file into the backup folder
-        shutil.copy2(f"{hitcircle_prefix}-{i}@2x.png", {backup_folder})
-
+        shutil.copy2(
+            f"{hitcircle_prefix}-{i}@2x.png",
+            f"{backup_folder}/{hitcircle_prefix}-{i}@2x.png",
+        )
     except FileNotFoundError:
         try:
             # Try to open the SD hitcircle number
             number = Image.open(f"{hitcircle_prefix}-{i}.png")
             # Copy the file into the backup folder
-            shutil.copy2(f"{hitcircle_prefix}-{i}.png", {backup_folder})
+            shutil.copy2(
+                f"{hitcircle_prefix}-{i}.png",
+                f"{backup_folder}/{hitcircle_prefix}-{i}.png",
+            )
         except FileNotFoundError:
             # Alert the user if the hitcircle number could not be found
             print(f"No default-{i}(@2x) could be found in the provided skin!")
-            sys.exit()
+            sys.exit()  # Move this line inside the inner except block
 
-for n in range(10):
-    paste = Image.open("no-number.png")
-    number = Image.open(f"before-instafader/default-{n}.png")
-    x, y = paste.size
+    # Paste the hitcircle number on top of the no-number image
+    no_number = Image.open("no-number.png")
+    x, y = no_number.size
     a, b = number.size
-    paste.paste(number, (x // 2 - a // 2, y // 2 - b // 2))
-    # paste.resize(160, 160, Image.Resampling.LANCZOS)
-    paste.save(f"default-{n}.png")
+    no_number.paste(number, ((x - a) // 2, (y - b) // 2), number)
+    no_number.save(f"default-{i}{'@2x' if hd else ''}.png")
 
-os.remove("no-number.png")  # remove no number image as we are done with it
 
-# create blank images for hitcircles
-blank_image = Image.new("RGB", (0, 0))
-blank_image.save("hitcircle.png")
-blank_image.save("hitcircleoverlay.png")
+config["Fonts"]["HitCircleOverlap"] = str(x // 2 if hd else x)
 
-config["Fonts"]["HitCirclePrefix"] = "default"
-config["Fonts"]["HitCircleOverlap"] = str(
-    x
-)  # update hitcircleoverlap in skin.ini
+for option in config["Colours"]:
+    if option.startswith("combo"):
+        if not option.startswith("combo1"):
+            del config["Colours"][option]
 
-with open("skin.ini", "w", encoding="utf-8") as f:  # save everything
-    config.write(f)
+config["Colours"]["Combo1"] = option[1]
+
+# Write all changes
+...
+
+
+# Todo:
+# - Ask if user wants to use sliderendcircle/overlay if it exists
+# - Do checks for HD assets and account for that
